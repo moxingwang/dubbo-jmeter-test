41fb0e936138b0791f0f4be03e053d07d79490b3
moxingwang <chris.mo@chinaredstar.com>
2018-06-07 20:28:48 +0800

    commit

--- src/main/java/com/mo/AbstractTesterSamplerClient.java
+++ src/main/java/com/mo/AbstractTesterSamplerClient.java
@@ -2,11 +2,29 @@ package com.mo;
 
 import com.mo.handler.ApplicationContextHandler;
 import org.apache.jmeter.protocol.java.sampler.AbstractJavaSamplerClient;
+import org.apache.jmeter.protocol.java.sampler.JavaSamplerContext;
 
 /**
  * @author MoXingwang on 2018-06-07.
  */
-public abstract class  AbstractTesterSamplerClient extends AbstractJavaSamplerClient {
-//    public ApplicationContextHandler initializer = ApplicationContextHandler.getInstance();
+public abstract class AbstractTesterSamplerClient extends AbstractJavaSamplerClient {
+
+    ApplicationContextHandler contextHandler;
+
+    /**
+     * 初始化所需要的Bean
+     * 如 (IQueryOrderService) contextHandler.getBean("queryOrderService");
+     */
+    public abstract void initBean();
+
+    @Override
+    public void setupTest(JavaSamplerContext javaSamplerContext) {
+        super.setupTest(javaSamplerContext);
+        contextHandler = ApplicationContextHandler.getInstance();
+        contextHandler.init();
+
+        //初始化bean
+        initBean();
+    }
 
 }
---
+++ src/main/java/com/mo/OrderServiceTest.java
@@ -0,0 +1,39 @@
+package com.mo;
+
+import com.chinaredstar.ordercenter.serviceinterface.order.IOrderService;
+import org.apache.jmeter.protocol.java.sampler.JavaSamplerContext;
+import org.apache.jmeter.samplers.SampleResult;
+
+import javax.annotation.Resource;
+
+/**
+ * @author MoXingwang on 2018-06-07.
+ */
+public class OrderServiceTest extends AbstractTesterSamplerClient {
+
+    @Resource
+    private IOrderService orderService;
+
+    public void initBean() {
+        orderService = (IOrderService) contextHandler.getBean("orderService");
+    }
+
+    public SampleResult runTest(JavaSamplerContext javaSamplerContext) {
+        SampleResult result = new SampleResult();
+        try {
+            long start = System.currentTimeMillis();
+
+            orderService.findById(1111111111111L);
+
+            result.setSuccessful(true);
+            long end = System.currentTimeMillis();
+            result.setResponseData((end - start) + "", null);
+        } finally {
+            result.sampleEnd();
+        }
+        return result;
+
+    }
+
+
+}
